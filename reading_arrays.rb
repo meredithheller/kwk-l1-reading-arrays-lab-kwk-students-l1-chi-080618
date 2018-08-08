02b13e1534c965e78dfebf700cf93f5e7048a030
meredithheller <meredithheller@outlook.com>
2018-08-08 15:23:48 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -19,26 +19,26 @@ end
 
 fourth_student_by_index
 
-# def last_student_by_index
-#   puts student_names[5]
-# end
+def last_student_by_index
+  puts student_names[5]
+end
 
-# last_student_by_index
+last_student_by_index
 
-# def first_student_by_method
-#   puts student_names.first
-# end
+def first_student_by_method
+  puts student_names.first
+end
 
-# first_student_by_method
+first_student_by_method
 
-# def last_student_by_method
-#   puts student_names.last
-# end
+def last_student_by_method
+  puts student_names.last
+end
 
-# last_student_by_method
+last_student_by_method
 
-# def first_second_and_third_students
-#   puts student_names[0...3]
-# end
+def first_second_and_third_students
+  puts student_names[0...3]
+end
 
-# first_second_and_third_students
+first_second_and_third_students
