8583dabbc6adb66397a21e78d7f988582505b98b
meredithheller <meredithheller@outlook.com>
2018-08-08 15:21:08 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -17,17 +17,25 @@ def fourth_student_by_index
   puts student_names[3]
 end
 
+fourth_student_by_index
+
 def last_student_by_index
   puts student_names[5]
 end
+
+last_student_by_index
 
 def first_student_by_method
   puts student_names.first
 end
 
+first_student_by_method
+
 def last_student_by_method
   puts student_names.last
 end
+
+last_student_by_method
 
 def first_second_and_third_students
   puts student_names[0...3]
